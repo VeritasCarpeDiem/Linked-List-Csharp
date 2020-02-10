37bf68ed70a8bbe5eab20e11baba8e2960fd2b91
Jin Kim <jinhong0425@gmail.com>
2020-02-10 12:21:20 -0800

    first commit

---
+++ Interface.cs
@@ -0,0 +1,181 @@
+﻿using System;
+class Program
+{
+
+    //interface ISpeakable
+    //{
+    //    void Speak();
+    //}
+
+
+    //class Animal : ISpeakable
+    //{
+    //    public void Speak()
+    //    {
+    //        Console.WriteLine("animal noise");
+    //    }
+    //}
+
+    //class Human : ISpeakable
+    //{
+    //    public void Speak()
+    //    {
+    //        Console.WriteLine("human noise");
+    //    }
+    //}
+
+    //class Car : ISpeakable
+    //{
+    //    public void Speak()
+    //    {
+    //        Console.WriteLine("car noise");
+    //    }
+    //}
+
+    //class B
+    //{
+
+    //}
+
+
+
+
+
+    //class Test : B, ISpeakable, IEnumerable, IDisposable
+    //{
+    //    ISpeakable speakable;
+    //    public Test(ISpeakable temp)
+    //    {
+    //        speakable = temp;
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public IEnumerator GetEnumerator()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void MakeNoise()
+    //    {
+    //        speakable.Speak();
+    //    }
+
+    //    public void Speak()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+
+    class Car : IComparable<Car>
+    {
+
+        public int year;
+
+        public Car(int year)
+        {
+
+            this.year = year;
+        }
+
+        public int CompareTo(Car other)
+        {
+            if (other == null || this.year > other.year)
+            {
+                return 1;
+            }
+
+            if (other.year == this.year)
+            {
+                return 0;
+            }
+
+            return -1;
+
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+
+
+        Car a = new Car(2017);
+        Car b = new Car(2019);
+        Car c = new Car(1991);
+        Car d = new Car(1886);
+
+
+        Car[] cars = new Car[] { a, b, c, d };
+
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car.year);
+        }
+
+
+        Console.WriteLine(new string('-', 100));
+        Array.Sort(cars);
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car.year);
+        }
+    }
+}
+
+            //Console.WriteLine(a.CompareTo(b));
+
+//new Test(new Car()).MakeNoise();
+
+////new Animal().Speak();
+
+////new Human().Speak();
+
+//ISpeakable[] speakables = new ISpeakable[] { new Animal(), new Human(), new Car() };
+
+//foreach (var speakable in speakables)
+//{
+//    speakable.Speak();
+//}
+
+//var test = new A();
+
+//foreach (var item in test)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//foreach (var item in "abc")
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//string input = Console.ReadLine();
+
+//Console.WriteLine(input == new string(input.ToCharArray().Reverse().ToArray()));
+
+//var list = new LinkedList();
+
+////  Console.WriteLine(list.Count);
+
+//list.AddLast("test");
+
+////  Console.WriteLine(list.RemoveLast());
+
+//list.AddLast("apple");
+//list.AddLast("orange");
+//list.AddLast("banana");
+
+
+//foreach (var item in list.OrderByDescending( x => x.Length))
+//{
+//    Console.WriteLine(item);
+//}
---
+++ Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;//lets me use generic variables
+
+namespace Linked_List_C_Sharp
+{
+    class Node
+    {
+        public string item;
+        public Node next;
+
+        public Node()
+        {
+
+        }
+        public Node(string item)
+        {
+            this.item = item;
+        }
+
+
+    }
+   public class LinkedList:IEnumerable<string>
+    {
+        private Node head;
+        private Node tail;
+        public int count; //item iterator
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        #region => operator
+        //public int Count =>count
+        #endregion
+        public int Count
+        {
+            get;
+            private set;
+        }
+        
+
+        public void displayList()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.item);
+
+                temp = temp.next;
+            }
+        }
+        public void addLast(string item)
+        {
+
+            Node temp = new Node(item);
+            if (head == null)
+            {
+                head = temp;
+                tail = temp;
+            }
+            else
+            {
+                tail.next = temp;
+                tail = temp;
+            }
+            count++;
+        }
+        public bool removeItem(string item)
+        {
+            Console.Write("Enter name of item to delete: ");
+            item = Console.ReadLine();
+
+
+            Node current = head.next;
+            Node previous = head;
+
+            if (head.item == item)//if item you want to delete is the 1st item
+            {
+                removeFirst();
+                return true;
+            }
+
+            while (current != null) //loop through list
+            {
+                if (current.item.Equals(item))
+                {
+                    previous.next = current.next;
+                    count--;
+                    return true;
+                }
+                previous = current;
+                current = current.next;
+            }
+
+
+            // current is the  node before
+            // get the node after next
+            // connect current to the node after next
+
+            return false;
+
+        }
+
+        public bool removeLast()
+        {
+            Node current = new Node();
+
+            if (head == tail)//if there us only 1 item in list
+            {
+                head = null;
+                tail = null;
+                count = 0;
+                return true;
+            }
+            current = head;
+
+            while (current != null)
+            {
+                if (current.next == tail)
+                {
+                    tail = current;
+                    current.next = null;
+                    count--;
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
+        }
+        public void removeFirst()
+        {
+            if (isEmpty())
+            {
+                Node temp = head;
+                head = temp.next;
+                count--;
+            }
+
+        }
+        public bool isEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            var current = head;
+            while(current != null)
+            {
+                yield return current.item;
+                current = current.next;
+            }
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempItem = "";
+            char reply = 'n';
+
+            Node myNode = new Node(tempItem);
+            var myList = new LinkedList();
+
+
+            myList.addLast("apple");
+            myList.addLast("orange");
+            myList.addLast("banana");
+
+            foreach (string item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(myList.count);
+            myList.displayList();
+
+            Console.WriteLine(myList.removeLast());
+            myList.displayList();
+            Console.WriteLine(myList.count);
+
+            #region static vs non static functions
+            //for static functions
+            //NameofClass.functionName
+
+            //for non static functions
+            //NameofClassObject.functionName
+            #endregion
+            #region main
+            //do
+            //{
+            //    myList.addItem(tempItem);
+            //    if (myList.count > 1)
+            //    {
+            //        Console.WriteLine("Delete item?(Y/N)");
+            //        reply = char.Parse(Console.ReadLine());
+            //    }
+            //    if (reply == 'y' || reply == 'Y')
+            //    {
+            //        myList.removeItem(tempItem);
+            //    }
+            //    myList.displayList();
+
+            //    Console.WriteLine("Add another item?(Y/N)");
+
+            //    reply = char.Parse(Console.ReadLine());
+
+            //} while (reply == 'y' || reply == 'Y');
+
+            #endregion
+        }
+    }
+
+}
